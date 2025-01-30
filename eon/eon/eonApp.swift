e803a6ebf14f64f7c02384dd703037d2497c21e3
Zubin <45184951+thezube1@users.noreply.github.com>
2025-01-30 14:11:25 -0800

    Init push

---
+++ eon/eon/eonApp.swift
@@ -0,0 +1,17 @@
+//
+//  eonApp.swift
+//  eon
+//
+//  Created by Zubin Hydrie on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct eonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
